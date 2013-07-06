7a248a9627c4ceb68a2693d090da6140715c005e
Giuseppe Sollazzo <puntofisso@gmail.com>
2013-07-06 22:11:07 +0100

    Committing API

--- www/api/index.php
+++ www/api/index.php
@@ -1,6 +1,8 @@
 <?php
 require('Toro.php');
 
+/* Generic Functions */
+
 function mySQLquery($query) {
 	$DB_HOST = "localhost";
 	$DB_USER = "ren";
@@ -18,15 +20,18 @@ function mySQLquery($query) {
 	return $result; 
 }
 
+/* API Endpoint Handlers */
+
 class RootHandler {
     function get() {
-      echo "BMJHack\n";
+      echo "BMJHack<br/>";
     }
 }
 
 class FsoHandler {
     function get() {
-      echo "Figuring Shit Out\n";
+      echo "Figuring Shit Out<br/>";
+      echo "I do very little, but a lot of it<br/>";
     }
 }
 
@@ -42,10 +47,99 @@ class TestHandler {
     }
 }
 
+class UserStatsHandler {
+    function get($username) {
+     	$query = "SELECT * from users where username='$username';";
+      	$result = mySQLquery($query);
+      	$out = array();
+      	while($row = mysql_fetch_assoc( $result )) {
+			$out['username'] = $row['username'];
+			$out['fullname'] = $row['fullname'];
+			$out['points'] = $row['points'];
+			$out['percentcorrect'] = $row['percentcorrect'];
+			$out['level'] = $row['level'];
+		}
+		echo json_encode($out);
+    }
+}
+
+class QuestionsHandler {
+    function get($catname) {
+
+    	$query = "";
+     	if ($catname == "all") {
+
+     		$query = "SELECT q.* from questions q where q.qid not in (SELECT questionid from answeredquestions WHERE username='$username') order by q.AVScore asc LIMIT 0,10;";
+  			
+
+
+  		} else {
+  			$query = "SELECT q.* from questions q where q.FriendlyName='$catname' and q.qid not in (SELECT questionid from answeredquestions WHERE username='$username') order by q.AVScore asc LIMIT 0,10;";
+
+  		}
+  		$result = mySQLquery($query);
+  		$out = array();
+
+
+  			while($row = mysql_fetch_assoc( $result )) {
+  				$thisout=array();
+		 		$thisout['qid'] = $row['QID'];
+		 		$thisout['category'] = $row['FriendlyName'];
+		 		$xml = $row['XML'];
+
+		 		// Extract XML
+		 		$dom = new DOMDocument;
+				$dom->loadXML($xml);
+
+				// question
+				$qText = $dom->getElementsByTagName('qTxt');
+				$string = "";	
+				foreach ($qText as $question) {
+    				foreach($question->childNodes as $child) {
+        				if ($child->nodeType == XML_CDATA_SECTION_NODE) {
+            				$string = $string . $child->textContent;
+        				}
+    				}
+				}
+				$thisout['question'] = strip_tags($string);
+
+				// answers
+				$opts = $dom->getElementsByTagName('o');
+				$optsArray = array();
+				foreach ($opts as $opt) {
+					$thisScore = array();
+					$thisScore['score'] = $opt->getAttribute('score');
+					$optiontext = $opt->getElementsByTagName('oTxt');
+					$optionstring = "";
+					foreach ($optiontext as $text) {
+    					foreach($text->childNodes as $child) {
+        					if ($child->nodeType == XML_CDATA_SECTION_NODE) {
+            					$optionstring = $optionstring . $child->textContent;
+        					}
+    					}
+					}
+					$thisScore['option'] = strip_tags($optionstring);
+					$optsArray[] = $thisScore;
+				}
+
+				$thisout['answers'] = $optsArray;
+
+		 		//$thisout['xml'] = $row['XML'];
+		 		$out[] = $thisout;
+			}
+	    	echo json_encode($out);
+	    
+    }
+}
+
+/* Toro main list of endpoints */
+
 Toro::serve(array(
     "/" => "RootHandler",
     "/fso" => "FsoHandler",
     "/test" => "TestHandler",
+    "/userstats/:string" => "UserStatsHandler",
+    "/questions/:string" => "QuestionsHandler",
 ));
 
 ?>
